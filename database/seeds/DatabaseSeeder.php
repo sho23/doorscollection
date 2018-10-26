328ad7f75718e0c8b5c8ecca2693607e6cf80df5
sho23 <testuser.shota@gmail.com>
2018-10-27 04:25:44 +0900

    テスト用ユーザーデータの追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+
+        // only for test
+        $this->call(UserTableSeeder::class);
     }
 }
