05a7af2387740c783aa3020e5e072941730b64e0
sho23 <testuser.shota@gmail.com>
2019-03-03 14:38:50 +0900

    バリデーションメッセージの日本語化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'メールアドレスとパスワードが一致していません。',
+    'throttle' => 'ログイン送信回数の上限を超えました。 :seconds 秒空けてからもう一度お試しください。',
+
+];
