0a9d9989a93ed2f318cf62495af1029903bbbe6f
sho23 <testuser.shota@gmail.com>
2019-03-04 02:21:14 +0900

    ログイン周りのレイアウトの修正

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title', 'グロリアの扉コレクション')
-@section('class', 'entrance-list')
+@section('class', 'single-page')
 @section('content')
 <div class="fixed-top">
     <header class="px-1">
@@ -14,43 +14,45 @@
         </ul>
     </header>
 </div>
-<div class="row justify-content-center p-4">
-    <div class="col-md-8">
-        <div class="card auth-form">
-            <div class="card-header">パスワードのリセット</div>
+<div class="container pt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card auth-form">
+                <div class="card-header">パスワードのリセット</div>
 
-            <div class="card-body">
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
-
-                <form method="POST" action="{{ route('password.email') }}">
-                    @csrf
-
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                            @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
                         </div>
-                    </div>
+                    @endif
 
-                    <div class="form-group row mb-0 mt-4">
-                        <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-success">
-                                パスワードを再設定
-                            </button>
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-5 col-form-label text-md-right">メールアドレス</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-                </form>
+
+                        <div class="form-group row mb-0 mt-4">
+                            <div class="col-md-8 offset-md-2">
+                                <button type="submit" class="btn btn-success">
+                                    パスワードを再設定
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.app')
-
+@section('title', 'グロリアの扉コレクション')
+@section('class', 'single-page')
 @section('content')
-<div class="container">
+<div class="fixed-top">
+    <header class="px-1">
+        <ul class="row">
+            <li class="col-2 pl-4 bars-menu"><a href="#sidr" id="simple-menu"><i class="fas fa-bars text-light"></i></a></li>
+            <li class="col-5 header-logo"><a href="/"><img src="{{ asset('image/logo_small.png') }}" alt="" hight="27px" width="107px"></a></li>
+            <li class="col-5 text-right pr-4">
+                <a href="#"><span class="p-2 badge badge-pill badge-warning"><i class="fas fa-bell text-light"></i></span></a>
+                <a href="{{ action('EntrancesController@mypage') }}"><span class="ml-2 p-2 badge badge-pill badge-warning"><i class="fas fa-user-alt text-light"></i></span></a>
+            </li>
+        </ul>
+    </header>
+</div>
+<div class="container pt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">メールの認証</div>
+        <div class="col-md-12">
+            <div class="card border-warning">
+                <div class="card-header bg-warning text-white">メールの認証</div>
 
                 <div class="card-body">
                     @if (session('resent'))
