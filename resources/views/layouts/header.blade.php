5cc6437f9a6ae50ddcd9eb4b65d0470ac696909d
sho23 <testuser.shota@gmail.com>
2019-03-15 01:33:21 +0900

    ロゴのURL遷移先をホームに変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <header class="px-1">
         <ul class="row">
             <li class="col-2 pl-4 bars-menu"><a href="#sidr" id="simple-menu"><i class="fas fa-bars text-light"></i></a></li>
-            <li class="col-5 header-logo"><a href="/"><img src="{{ asset('image/logo_small.png') }}" alt="" hight="27px" width="107px"></a></li>
+            <li class="col-5 header-logo"><a href="{{ action('HomeController@index') }}"><img src="{{ asset('image/logo_small.png') }}" alt="" hight="27px" width="107px"></a></li>
             <li class="col-5 text-right pr-4 dropdown">
                 <a href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="p-2 badge badge-pill badge-warning"><i class="fas fa-bell text-light"></i></span></a>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
