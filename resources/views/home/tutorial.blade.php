e377c98176ddd20d0892cda16d7e6e18eadb25b7
sho23 <testuser.shota@gmail.com>
2019-04-17 02:03:17 +0900

    bootstrapのバージョンアップと余白の調整

--- resources/views/home/tutorial.blade.php
+++ resources/views/home/tutorial.blade.php
@@ -24,7 +24,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -114,12 +114,11 @@
                     <div class="carousel-item text-center">
                         <div class="d-flex tutorial-header">
                             <span class="ribbon">使い方</span>
-                            <a href="{{ action('HomeController@index') }}" class="btn btn btn-outline-info">SKIP</a>
                         </div>
                         <div class="m-2">
                             <img src="{{ asset('image/tutorial/tutorial06.png') }}">
                             <div class="tutorial-footer">
-                                <a href="{{ action('HomeController@index') }}" class="btn btn-success btn-sm my-3">さぁ、はじめまショウ！</a>
+                                <a href="{{ action('HomeController@index') }}" class="btn btn-success my-3">さぁ、はじめまショウ！</a>
                             </div>
                         </div>
                     </div>
@@ -134,12 +133,7 @@
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script>
-        $(function() {
-            $("div[data-slide-to='0'].active").parent().html();
-        });
-    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
