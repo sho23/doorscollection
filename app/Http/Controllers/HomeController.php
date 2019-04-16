92cfc8a97a19be29bf65f5878e663c8bfa6eae20
sho23 <testuser.shota@gmail.com>
2019-04-17 02:50:01 +0900

    使い方ページヘの導線を追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'policy', 'qa', 'terms']);
+        $this->middleware('auth')->except(['index', 'policy', 'qa', 'terms', 'tutorial']);
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@
                 <li class="text-center p-2"><img src="{{ asset('image/ic_gloria.png') }}" class="rounded-circle" alt="" width="100"></li>
                 <li class="text-center"><a href="{{ action('HomeController@index') }}">ホーム</a></li>
                 <!-- <li class="text-center"><a href="#">お知らせ</a></li> -->
+                <li class="text-center"><a href="{{ action('HomeController@tutorial') }}">使い方</a></li>
                 <li class="text-center"><a href="{{ action('HomeController@policy') }}">プライバシーポリシー</a></li>
                 <li class="text-center"><a href="{{ action('HomeController@qa') }}">Q&A</a></li>
                 <li class="text-center"><a href="http://team-jokers.tokyo" target="_blank">運営会社</a></li>
